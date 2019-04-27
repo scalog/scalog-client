cfd5180773f073c2da5e57b07d373a68db77de33
Ryan Yoon <ryandyoon@gmail.com>
2019-04-27 17:54:25 -0400

    subscribe implemented with chan instead of callback

--- client/client.go
+++ client/client.go
@@ -1,4 +1,4 @@
-/*Package client provides clients with the ability to interact with the Scalog API. */
+// Package client provides clients with the ability to interact with the Scalog API.
 package client
 
 import (
@@ -15,45 +15,63 @@ import (
 )
 
 /*
- * Client is a struct that carries the meta-data necessary for a client to interact
- * with the Scalog API. Clients should initialize a new instance of this struct with
- * the newClient() function.
- */
+SubscribeResponse is a struct that represents a record [Record] that has been ordered
+by Scalog and assigned the global sequence number [gsn].
+*/
+type SubscribeResponse struct {
+	Gsn    int32
+	Record string
+}
+
+/*
+Client is a struct that carries the meta-data necessary for a client to interact
+with the Scalog API. Clients should initialize a new instance of this struct with
+the newClient() function.
+*/
 type Client struct {
 	// Integer serving as unique client id
 	cid int32
 	// Client-generated record sequence number
 	csn int32
 	// Mutex for interacting with [csn]
-	mu sync.RWMutex
+	cmu sync.RWMutex
+	// Next global sequence number to respond to client
+	nextGsn int32
+	// Set of global sequence numbers received from data servers
+	ss *set64.Set64
+	// Map from global sequence number to [SubscribeResponse]
+	sm map[int32]SubscribeResponse
+	// Mutex for interacting with [nextGsn], [ss], and [sm]
+	smu sync.RWMutex
+	// Channel to send subscribe responses to the client
+	sc chan SubscribeResponse
 }
 
 /*
- * Function that is called when a data server reports an ordered record to the
- * client after the client has subscribed.
- */
-type Callback func(int32, string, error)
-
-/*
- * Initializes and returns a new instance of [Client] with a unique client id.
- */
+NewClient initializes and returns a new instance of [Client] with a unique client id.
+*/
 func NewClient() *Client {
-	set64.NewSet64()
 	return &Client{
-		cid: assignClientID(),
-		csn: 0,
-		mu:  sync.RWMutex{},
+		cid:     assignClientID(),
+		csn:     0,
+		cmu:     sync.RWMutex{},
+		nextGsn: -1,
+		ss:      set64.NewSet64(),
+		sm:      make(map[int32]SubscribeResponse),
+		smu:     sync.RWMutex{},
+		sc:      make(chan SubscribeResponse),
 	}
 }
 
 /*
- * Appends a record [r], and returns the global sequence number assigned to it.
- */
+Append appends a record [r], and returns the global sequence number assigned to it.
+Note: this is a blocking function!
+*/
 func (c *Client) Append(r string) (int32, error) {
 	var opts []grpc.DialOption
 	opts = append(opts, grpc.WithInsecure())
-	addresses := discoverServers("130.127.133.24:32403") // TODO: move port to config file
-	address := appendPlacementPolicy(addresses)
+	addresses := discoverServers("130.127.133.24:32403") // TODO: move address to config file
+	address := applyAppendPlacementPolicy(addresses)
 	conn, err := grpc.Dial(addressToString(address), opts...)
 	if err != nil {
 		panic(err)
@@ -61,14 +79,15 @@ func (c *Client) Append(r string) (int32, error) {
 	defer conn.Close()
 
 	dataClient := data.NewDataClient(conn)
-	c.mu.Lock()
+	c.cmu.Lock()
 	appendRequest := &data.AppendRequest{
 		Cid:    c.cid,
 		Csn:    c.csn,
 		Record: r,
 	}
 	c.csn = c.csn + 1
-	c.mu.Unlock()
+	c.cmu.Unlock()
+
 	resp, err := dataClient.Append(context.Background(), appendRequest)
 	if err != nil {
 		return -1, err
@@ -77,38 +96,41 @@ func (c *Client) Append(r string) (int32, error) {
 }
 
 /*
- * Subscribes to records starting from global sequence number [gsn].
- */
-func (c *Client) Subscribe(gsn int32, callback Callback) {
-	addresses := discoverServers("130.127.133.24:32403") // TODO: move port to config file
+Subscribe subscribes to records starting from global sequence number [gsn].
+*/
+func (c *Client) Subscribe(gsn int32) chan SubscribeResponse {
+	c.cmu.Lock()
+	c.nextGsn = gsn
+	c.cmu.Unlock()
+	addresses := discoverServers("130.127.133.24:32403") // TODO: move address to config file
 	for _, address := range addresses {
-		go c.subscribe(address, gsn, callback)
+		go c.subscribe(address, gsn)
 	}
+	return c.sc
 }
 
 /*
- * Deletes records before global sequence number [gsn].
- */
+Trim deletes records before global sequence number [gsn].
+*/
 func (c *Client) Trim(gsn int32) {
-	addresses := discoverServers("130.127.133.24:32403") // TODO: move port to config file
+	addresses := discoverServers("130.127.133.24:32403") // TODO: move address to config file
 	for _, address := range addresses {
 		go c.trim(address, gsn)
 	}
 }
 
 /*
- * Queries the discovery service and returns the unique client id assigned.
- */
+Returns a randomly generated 31-bit integer as int32.
+Note: uniqueness is not guaranteed!
+*/
 func assignClientID() int32 {
-	// TODO: query discovery service to obtain client id in order to ensure that
-	// client ids are unique
 	return rand.Int31()
 }
 
 /*
- * Queries the discovery service and returns the ports of the active data
- * servers as a slice.
- */
+Queries the discovery service and returns a slice of the addresses of all active
+data servers.
+*/
 func discoverServers(address string) []*discovery.DataServerAddress {
 	var opts []grpc.DialOption
 	opts = append(opts, grpc.WithInsecure())
@@ -120,6 +142,7 @@ func discoverServers(address string) []*discovery.DataServerAddress {
 
 	discoveryClient := discovery.NewDiscoveryClient(conn)
 	discoveryRequest := &discovery.DiscoverRequest{}
+
 	resp, err := discoveryClient.DiscoverServers(context.Background(), discoveryRequest)
 	if err != nil {
 		panic(err)
@@ -128,22 +151,22 @@ func discoverServers(address string) []*discovery.DataServerAddress {
 }
 
 /*
- * Given a slice of addresses of the active data servers, selects and returns a
- * single address based on the data placement policy.
- */
-func appendPlacementPolicy(addresses []*discovery.DataServerAddress) *discovery.DataServerAddress {
+Given a slice of addresses of all active data servers, selects and returns a single
+address based on the pre-defined data placement policy.
+*/
+func applyAppendPlacementPolicy(addresses []*discovery.DataServerAddress) *discovery.DataServerAddress {
 	if len(addresses) == 0 {
 		panic("Failed to append: no active data servers discovered!")
 	}
-	return addresses[rand.Intn(len(addresses))] // TODO: select port based on data placement policy
+	return addresses[rand.Intn(len(addresses))] // TODO: select address based on data placement policy
 }
 
 /*
- * Creates a stream to the data server on port [port], listens for the data
- * data server to report ordered records with gsn > [gsn], and passes those
- * arguments to the [callback] function.
- */
-func (c *Client) subscribe(address *discovery.DataServerAddress, gsn int32, callback Callback) {
+Creates a stream to the data server on address [address], listens for the
+data server to report ordered records with gsn > [gsn], and if possible,
+responds to the client in order.
+*/
+func (c *Client) subscribe(address *discovery.DataServerAddress, gsn int32) {
 	var opts []grpc.DialOption
 	opts = append(opts, grpc.WithInsecure())
 	conn, err := grpc.Dial(addressToString(address), opts...)
@@ -153,30 +176,53 @@ func (c *Client) subscribe(address *discovery.DataServerAddress, gsn int32, call
 	defer conn.Close()
 
 	dataClient := data.NewDataClient(conn)
-	r := &data.SubscribeRequest{
+	subscribeRequest := &data.SubscribeRequest{
 		SubscriptionGsn: gsn,
 	}
-	stream, err := dataClient.Subscribe(context.Background(), r)
+
+	stream, err := dataClient.Subscribe(context.Background(), subscribeRequest)
 	if err != nil {
 		panic(err)
 	}
-
-	for true {
+	for {
 		in, err := stream.Recv()
 		if err == io.EOF {
 			return
 		}
 		if err != nil {
-			callback(-1, "", err)
+			panic(err)
 		}
-		callback(in.Gsn, in.Record, nil) // TODO: filter duplicate gsn
+
+		c.smu.Lock()
+		c.ss.Add(int64(in.Gsn)) // TODO: remove type casting once gsn are int64
+		c.sm[in.Gsn] = SubscribeResponse{
+			Gsn:    in.Gsn,
+			Record: in.Record,
+		}
+		c.smu.Unlock()
+		c.respond()
 	}
 }
 
 /*
- * Creates a stream to the data server on port [port] and requests that
- * all records with gsn < [gsn] be deleted.
- */
+Responds to client with [SubscribeResponse] in order of global sequence number
+if possible.
+*/
+func (c *Client) respond() {
+	c.cmu.Lock()
+	for c.ss.Contains(int64(c.nextGsn)) { // TODO: remove type casting once gsn are int64
+		c.sc <- c.sm[c.nextGsn]
+		c.ss.Remove(int64(c.nextGsn)) // TODO: remove type casting once gsn are int64
+		delete(c.sm, c.nextGsn)
+		c.nextGsn = c.nextGsn + 1
+	}
+	c.cmu.Unlock()
+}
+
+/*
+Creates a stream to the data server on address [address] and requests that all
+records with gsn < [gsn] be deleted.
+*/
 func (c *Client) trim(address *discovery.DataServerAddress, gsn int32) {
 	var opts []grpc.DialOption
 	opts = append(opts, grpc.WithInsecure())
@@ -190,10 +236,14 @@ func (c *Client) trim(address *discovery.DataServerAddress, gsn int32) {
 	r := &data.TrimRequest{
 		Gsn: gsn,
 	}
+
 	dataClient.Trim(context.Background(), r)
 	// No response expected
 }
 
+/*
+Returns DataServerAddress [address] as a string.
+*/
 func addressToString(address *discovery.DataServerAddress) string {
 	return fmt.Sprintf("%s:%d", address.Ip, address.Port)
 }
