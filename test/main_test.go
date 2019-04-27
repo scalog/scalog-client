50c2a471e11b05366eaf0db7455a690638a475ac
Ryan Yoon <ryandyoon@gmail.com>
2019-04-27 18:13:32 -0400

    added sanity test for subscribe

--- test/main_test.go
+++ test/main_test.go
@@ -11,7 +11,18 @@ func TestClientAppend(t *testing.T) {
 	client := client.NewClient()
 	resp, err := client.Append("Hello, World!")
 	if err != nil {
-		fmt.Println(err)
+		t.Errorf(err.Error())
 	}
-	fmt.Println(fmt.Sprintf("Global sequence number: %d", resp))
+	fmt.Println(fmt.Sprintf("Append responded with global sequence number: %d", resp))
+}
+
+func TestClientSubscribe(t *testing.T) {
+	client := client.NewClient()
+	c := client.Subscribe(0)
+	_, err := client.Append("Hello, World!")
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	resp := <-c
+	fmt.Println(fmt.Sprintf("Subscribe responded with global sequence number: %d", resp.Gsn))
 }
