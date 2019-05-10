7b605a9eda6494e280683070bc4b0b47fed2c60f
Ryan Yoon <ryandyoon@gmail.com>
2019-05-10 17:27:27 -0400

    added additional append and subscribe tests

--- test/main_test.go
+++ test/main_test.go
@@ -5,26 +5,85 @@ import (
 	"testing"
 
 	"github.com/scalog/scalog-client/client"
+	"github.com/scalog/scalog/pkg/set64"
 )
 
-func TestClientAppend(t *testing.T) {
+func TestSingleAppend(t *testing.T) {
 	client := client.NewClient()
-	resp, err := client.Append("Hello, World!")
+	gsn, err := client.Append("Hello, World!")
 	if err != nil {
-		t.Errorf(err.Error())
+		t.Fatalf(err.Error())
 	}
-	fmt.Println(fmt.Sprintf("Append responded with global sequence number: %d", resp))
+	if gsn < 1 {
+		t.Fatalf("Record assigned invalid global sequence number %d", gsn)
+	}
 }
 
-func TestClientSubscribe(t *testing.T) {
+func TestMultipleAppend(t *testing.T) {
+	client := client.NewClient()
+	set := set64.NewSet64()
+	for i := 1; i <= 5; i++ {
+		gsn, err := client.Append(fmt.Sprintf("Appending %d", i))
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+		if gsn < 1 {
+			t.Fatalf("Record assigned invalid global sequence number %d", gsn)
+		}
+		if set.Contains(int64(gsn)) {
+			t.Fatalf("Record assigned previously assigned global sequence number %d", gsn)
+		}
+		set.Add(int64(gsn))
+	}
+}
+
+func TestMultipleClientsAppend(t *testing.T) {
+	set := set64.NewSet64()
+	for i := 1; i <= 5; i++ {
+		client := client.NewClient()
+		gsn, err := client.Append(fmt.Sprintf("Appending %d", i))
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+		if gsn < 1 {
+			t.Fatalf("Record assigned invalid global sequence number %d", gsn)
+		}
+		if set.Contains(int64(gsn)) {
+			t.Fatalf("Record assigned previously assigned global sequence number %d", gsn)
+		}
+		set.Add(int64(gsn))
+	}
+}
+
+func TestSimpleSubscribe(t *testing.T) {
 	client := client.NewClient()
 	c := client.Subscribe(1)
-	appendResp, err := client.Append("Hello, World!")
-	fmt.Println(fmt.Sprintf("Append responded with global sequence number: %d", appendResp))
+	_, err := client.Append("Hello, World!")
 	if err != nil {
 		t.Errorf(err.Error())
 	}
-	for resp := range c {
-		fmt.Println(fmt.Sprintf("Subscribe responded with global sequence number: %d", resp.Gsn))
+	resp := <-c
+	if resp.Gsn < 1 {
+		t.Fatalf("Record assigned invalid global sequence number %d", resp.Gsn)
+	}
+}
+
+func TestMultipleSubscribe(t *testing.T) {
+	client := client.NewClient()
+	c := client.Subscribe(1)
+	set := set64.NewSet64()
+	for i := 1; i <= 5; i++ {
+		_, err := client.Append(fmt.Sprintf("Appending %d", i))
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+		resp := <-c
+		if resp.Gsn < 1 {
+			t.Fatalf("Record assigned invalid global sequence number %d", resp.Gsn)
+		}
+		if set.Contains(int64(resp.Gsn)) {
+			t.Fatalf("Duplicate subscribe responses received for global sequence number %d", resp.Gsn)
+		}
+		set.Add(int64(resp.Gsn))
 	}
 }
