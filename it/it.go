4a65520c0054a1485f697039c5a051ec6294a39b
Cong Ding <tuxding@gmail.com>
2019-05-13 13:36:56 -0400

    it: initial version

--- it/it.go
+++ it/it.go
@@ -1,6 +1,13 @@
 package it
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+
 	clientlib "github.com/scalog/scalog-client/lib"
 )
 
@@ -15,6 +22,37 @@ func NewIt() *It {
 }
 
 func (it *It) Start() error {
-	// TODO: implement the interactive client
+	regex := regexp.MustCompile(" +")
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		cmdString, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+		cmdString = strings.TrimSuffix(cmdString, "\n")
+		cmdString = strings.Trim(cmdString, " ")
+		cmd := regex.Split(cmdString, -1)
+		if len(cmd) != 2 {
+			fmt.Fprintln(os.Stderr, "Command error")
+			continue
+		}
+		if cmd[0] == "append" {
+			gsn, err := it.client.Append(cmd[2])
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				continue
+			}
+			fmt.Println(gsn)
+		} else if cmd[0] == "trim" {
+			gsn, err := strconv.ParseInt(cmd[2], 10, 32)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				continue
+			}
+			it.client.Trim(int32(gsn))
+		}
+	}
 	return nil
 }
