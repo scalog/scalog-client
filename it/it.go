84095d8c6d0fdc686769b7f329475eae33881d4b
Ryan Yoon <ryandyoon@gmail.com>
2019-05-19 14:25:13 -0400

    added support for appendToShard and readRecord to client it

--- it/it.go
+++ it/it.go
@@ -55,6 +55,18 @@ func (it *It) Start() error {
 				continue
 			}
 			fmt.Fprintf(os.Stderr, "Append result: { Gsn: %d, Record: %s }\n", gsn, record)
+		} else if strings.EqualFold(cmd[0], "appendToShard") {
+			if len(cmd) < 2 {
+				fmt.Fprintln(os.Stderr, "Command error: missing required argument [record]")
+				continue
+			}
+			record := strings.Join(cmd[1:], " ")
+			gsn, shardID, err := it.client.AppendToShard(record)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				continue
+			}
+			fmt.Fprintf(os.Stderr, "AppendToShard result: { Gsn: %d, Record: %s }, shardID: %d\n", gsn, record, shardID)
 		} else if cmd[0] == "subscribe" {
 			if len(cmd) < 2 {
 				fmt.Fprintln(os.Stderr, "Command error: missing required argument [gsn]")
@@ -79,6 +91,35 @@ func (it *It) Start() error {
 					fmt.Fprintf(os.Stderr, "Subscribe result: { Gsn: %d, Record: %s }\n", committedRecord.Gsn, committedRecord.Record)
 				}
 			}()
+		} else if strings.EqualFold(cmd[0], "readRecord") {
+			if len(cmd) < 3 {
+				fmt.Fprintln(os.Stderr, "Command error: missing required arguments [gsn] [shardID]")
+				continue
+			}
+			gsn, err := strconv.ParseInt(cmd[1], 10, 32)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				continue
+			}
+			if gsn < 1 {
+				fmt.Fprintln(os.Stderr, "Command error: [gsn] must be greater than 0")
+				continue
+			}
+			shardID, err := strconv.ParseInt(cmd[2], 10, 32)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				continue
+			}
+			if shardID < 0 {
+				fmt.Fprintln(os.Stderr, "Command error: [shardID] must be greater than or equal to 0")
+				continue
+			}
+			record, err := it.client.ReadRecord(int32(gsn), int32(shardID))
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				continue
+			}
+			fmt.Fprintf(os.Stderr, "ReadRecord result: { Record: %s }\n", record)
 		} else if cmd[0] == "trim" {
 			if len(cmd) < 2 {
 				fmt.Fprintln(os.Stderr, "Command error: missing required argument [gsn]")
